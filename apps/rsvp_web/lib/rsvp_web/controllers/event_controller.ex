10a057b40c0820e002ce363fae6eb456fcab88ab
Daniel Martínez Sarta <daniel@danielmartinez.dev>
2020-05-20 20:56:07 -0400

    Event list route created. Templates and sub-templates example. Route helpers example.

--- apps/rsvp_web/lib/rsvp_web/controllers/event_controller.ex
+++ apps/rsvp_web/lib/rsvp_web/controllers/event_controller.ex
@@ -11,5 +11,10 @@ defmodule RsvpWeb.EventController do
 		render conn, "details.html", event: event
 	end
 
+	def list(conn, _params) do
+		events = Rsvp.EventQueries.get_all()
+		render conn, "list.html", events: events
+	end
+
 
 end
